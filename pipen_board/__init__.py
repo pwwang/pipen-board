719e987378ed0b2159a4e9c334cc7305f74fd431
pwwang <pwwang@pwwang.com>
2023-04-18 10:19:48 -0700

    🔖 0.0.7

--- pipen_board/__init__.py
+++ pipen_board/__init__.py
@@ -1,7 +1,7 @@
 from .cli import PipenCliBoardPlugin
 from .plugin import PipenBoardPlugin
 
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 
 PipenBoardPlugin.__version__ = __version__
 PipenCliBoardPlugin.__version__ = __version__
