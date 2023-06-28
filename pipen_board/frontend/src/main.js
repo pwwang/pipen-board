0f4064254f125e1871ce1f4b05330580c0366991
pwwang <pwwang@pwwang.com>
2023-06-28 00:01:28 -0700

    🔖 0.7.7

--- pipen_board/frontend/src/main.js
+++ pipen_board/frontend/src/main.js
@@ -3,6 +3,7 @@ import "carbon-components-svelte/css/white.css"
 import './assets/markdown.css'
 import 'highlight.js/styles/github.css';
 import './assets/global.css'
+// @ts-ignore
 import App from './App.svelte'
 
 const app = new App({
