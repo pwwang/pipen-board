5f4bbb695f92dfb624711b3d6a306184f7843921
justold <1188067+pwwang@users.noreply.github.com>
2023-08-08 18:18:18 +0000

    👷 Update codesandbox scripts

--- .codesandbox/setup.sh
+++ .codesandbox/setup.sh
@@ -1,7 +1,7 @@
 WORKSPACE="/workspace"
 
 # Install python dependencies
-poetry update && poetry install -E report
+poetry update && poetry install -E report -E diagram
 
 # Install frontend dependencies for pipen-report
 pipen report update
@@ -11,3 +11,7 @@ cd $WORKSPACE/pipen_board/frontend
 npm install
 
 cd $WORKSPACE
+
+# Install whichpy
+WHICHPY="https://gist.githubusercontent.com/pwwang/879966128b0408c2459eb0a0b413fa69/raw/2f2573d191edec1937a2bf0873aa33a646b5ef29/whichpy.fish"
+curl -sS $WHICHPY -o ~/.config/fish/functions/whichpy.fish
