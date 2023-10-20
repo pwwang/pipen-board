7fa86878686fadb05572944c983f4bddd89f2979
justold <1188067+pwwang@users.noreply.github.com>
2023-10-20 15:47:30 +0000

    Remove from_pipen_board, use pipen.utils.is_loading_pipeline() instead

--- pipen_board/__init__.py
+++ pipen_board/__init__.py
@@ -4,9 +4,3 @@ from .plugin import PipenBoardPlugin
 
 # Need the instance to make self work
 pipen_board_plugin = PipenBoardPlugin()
-
-
-def from_pipen_board():
-    import sys
-
-    return sys.argv[0] == "@pipen-board"
